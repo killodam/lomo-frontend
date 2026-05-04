df5bca35ba1a5f72590c334363e074921f9a9762
Anton Sergeev <loskaban.jr@gmail.com>
2026-05-04 18:42:27 +0300

    Use same-origin API and chat endpoints

--- config.js
+++ config.js
@@ -1,11 +1,6 @@
 (function initLomoConfig() {
-  var host = window.location.hostname || '';
-  var isLocalHost = host === 'localhost' || host === '127.0.0.1';
-  var isLocalFile = window.location.protocol === 'file:';
-  var defaultApiBase = (isLocalHost || isLocalFile)
-    ? 'https://lomo-backend-hergg.amvera.io/api'
-    : '/api';
-  var defaultChatWsBase = 'https://lomo-backend-hergg.amvera.io';
+  var defaultApiBase = '/api';
+  var defaultChatWsBase = window.location.origin;
 
   window.LOMO_CONFIG = Object.assign(
     {
